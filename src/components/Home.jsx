686d2b83078de76beb3657da15d10cd2f9b6575c
AmitRaj2001 <amit.12018323@lpu.in>
2023-08-12 15:03:02 +0530

    modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Home.css 	modified:   src/components/Home.jsx 	modified:   src/components/Navbar.css 	new file:   src/components/Task.css 	new file:   src/components/Task.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,57 @@
+
 import React from "react";
 import "./Home.css";
+import Task from "./Task";
 
 const Home = () => {
-    return <div className="container">
-        <h1>Daily Goals</h1>
-        <form >
-            <input type="text" placeholder="Tasks"/>
-            <textarea placeholder="Description"></textarea>
-            <button type="submit">ADD</button>
-        </form>
-    </div>;
+    const initialArray = localStorage.getItem("tasks")?JSON.parse(localStorage.getItem("tasks")):[];
+
+    const [tasks, setTasks] = React.useState(initialArray);
+    const [title, setTitle] = React.useState("");
+    const [description, setDescription] = React.useState("");
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const newTask = { title, description };
+        setTasks([...tasks, newTask]);
+    };
+
+    React.useEffect(() => {
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+    }, [tasks]);
+
+
+
+    const deleteTask = (index) => {
+        const filteredTasks = tasks.filter((item, i) => i !== index);
+        setTasks(filteredTasks);
+    };
+
+    return (
+        <div className="container">
+            <h1>Daily Goals</h1>
+            <form onSubmit={submitHandler}>
+                <input
+                    type="text"
+                    placeholder="Tasks"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <textarea
+                    placeholder="Description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+                <button type="submit">ADD</button>
+            </form>
+            {tasks.map((item,index) => (
+                <Task key = {index} title={item.title} description={item.description}
+                deleteTask={deleteTask} 
+                index={index}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default Home;
